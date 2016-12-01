d6ebcbece8596ac855eb904d6305039a154abe67
Daniel Gonzalez <ogonzalez29@users.noreply.github.com>
2016-12-01 17:42:02 -0500

    created editor input files and text widgets with php information

---
+++ wp-content/themes/Kyma-child/assets/discounts-widget.php
@@ -0,0 +1,1 @@
+<p>Ofrecemos promociones y descuentos constantemente. Asegúrate de revisar esta sección seguido!<a href="<?php echo esc_url(home_url('/'));?>promociones/"> Ver más »</a></p>
---
+++ wp-content/themes/Kyma-child/assets/payment-methods.php
@@ -0,0 +1,14 @@
+<?php
+$visa = get_template_directory_uri() . '/../Kyma-child/images/visa.png';
+$masterCard = get_template_directory_uri() . '/../Kyma-child/images/mastercard.png';
+$diners = get_template_directory_uri() . '/../Kyma-child/images/diners.png';
+$amex = get_template_directory_uri() . '/../Kyma-child/images/americanexpress.png';
+$colsubsidio = get_template_directory_uri() . '/../Kyma-child/images/colsubsidio.png';
+?>
+<ul>
+	<li><img src=<?php echo $visa?> alt=""></li>
+	<li><img src=<?php echo $masterCard?> alt=""></li>
+	<li><img src=<?php echo $diners?> alt=""></li>
+	<li><img src=<?php echo $amex?> alt=""></li>
+	<li><img src=<?php echo $colsubsidio?> alt=""></li>
+</ul>
