4739aeaac82a6eb2b8750444db6ae1717ef8ed25
Daniel Gonzalez <ogonzalez29@users.noreply.github.com>
2016-12-01 17:42:02 -0500

    created editor input files and text widgets with php information

---
+++ wp-content/themes/Kyma-child/assets/location-map-widget.php
@@ -0,0 +1,8 @@
+<div class="widget_overlay" onclick="style.pointerEvents='none'"></div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3976.594978096009!2d-74.06535628481947!3d4.66607494325504!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f9a5870c8ea05%3A0x663c5856d3324ece!2sServitalleres!5e0!3m2!1ses-419!2sco!4v1480098372666" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+<div class="contactwidget">
+<p><i class= "fa fa-map-marker"></i>  Carrera 22 # 76 - 57, Bogotá - Colombia</p>
+<p><i class= "fa fa-phone"></i><a href="tel:+5712119290">  2117943 - 2119290</a></p>
+<p><i class= "fa fa-envelope"></i><a href="<?php echo esc_url(home_url('/'));?>contactanos/">  Formulario de contacto</a></p>
+</div>
+<div class="widget_button"><button>Agendar cita</button></div>
