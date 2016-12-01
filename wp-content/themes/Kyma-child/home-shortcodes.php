9fe0c05d1cd556fdc3d9eb0c2491bed18ca1c347
Daniel Gonzalez <ogonzalez29@users.noreply.github.com>
2016-12-01 16:53:46 -0500

    added promotions section to home via default shortcodes and added map home section

--- wp-content/themes/Kyma-child/home-shortcodes.php
+++ wp-content/themes/Kyma-child/home-shortcodes.php
@@ -2,13 +2,12 @@
 <section class="content_section bg_gray">
     <div class="content row_spacer no_padding">
         <div class="main_title centered upper"><?php if ($kyma_theme_options['home_extra_title'] != "") { ?>
-                <h2 id='blog-heading'><span class="line"><i class="fa fa-map-marker"></i></span><?php echo esc_attr($kyma_theme_options['home_extra_title']); ?>
+                <h2 id='blog-heading'><span class="line"><i class="fa fa-tags"></i></span><?php echo esc_attr($kyma_theme_options['home_extra_title']); ?>
                 </h2>
             <?php } ?>
         </div>
         <div class="rows_container clearfix">
             <div class="hm_blog_grid">
-                <div class="overlay" onClick="style.pointerEvents='none'"></div>
 				<?php echo do_shortcode($kyma_theme_options['home-shortcode']); ?>
 			</div>
 		</div>
