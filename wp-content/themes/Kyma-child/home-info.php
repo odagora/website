1888c7e7029b974cc8ed616dfe12f8229b7ac8fe
Daniel Gonzalez <oscar.d.gonzalez@gmail.com>
2016-12-16 10:50:05 -0500

    added info panel to homepage

---
+++ wp-content/themes/Kyma-child/home-info.php
@@ -0,0 +1,60 @@
+<?php $kyma_theme_options = kyma_theme_options();
+switch ($kyma_theme_options['home_info_type']) {
+    case 1:
+        ?>
+        <section id='callout' class="content_section white_section bg_color3">
+            <div class="welcome_banner full_colored">
+                <div class="content clearfix">
+                    <?php if ($kyma_theme_options['info_title'] != "") { ?>
+                        <h3 id='callout-title'><?php echo esc_attr($kyma_theme_options['info_title']); ?></h3>
+                    <?php } ?>
+                    <div class="callout_btn_a">
+                        <a href="" class="btn_a f_right">
+                    <span><i class="in_left <?php echo esc_attr($kyma_theme_options['callout_btn_icon']); ?>"></i><span
+                            id='callout-btn-text'><?php echo esc_attr($kyma_theme_options['callout_btn_text']); ?></span><i
+                            class="in_right <?php echo esc_attr($kyma_theme_options['callout_btn_icon']); ?>"></i></span>
+                        </a>
+                    </div><?php if ($kyma_theme_options['info_description'] != "") { ?>
+                            <span
+                                class="intro_text"><?php echo esc_attr($kyma_theme_options['info_description']); ?></span><?php } ?>
+                </div>
+            </div>
+        </section>
+        <?php
+        break;
+    case 2:
+        ?>
+        <!-- Intro Banner -->
+        <div class="welcome_banner full_gray" style="background: #324545;">
+            <div class="content clearfix">
+                <?php if ($kyma_theme_options['info_description'] != "") { ?>
+                    <h3><?php echo esc_attr($kyma_theme_options['info_description']); ?></h3>
+                <?php } ?>
+                <a href="<?php echo esc_url($kyma_theme_options['callout_btn_link']); ?>" <?php if($kyma_theme_options['callout_button_target']){ ?>target="_blank"<?php } ?>
+                   class="btn_a f_right">
+                    <span><i
+                            class="in_left <?php echo esc_attr($kyma_theme_options['callout_btn_icon']); ?>"></i><span><?php echo esc_attr($kyma_theme_options['callout_btn_text']); ?></span><i
+                            class="in_right <?php echo esc_attr($kyma_theme_options['callout_btn_icon']); ?>"></i></span>
+                </a>
+            </div>
+        </div>
+        <!-- End Intro Banner -->
+        <?php
+        break;
+    case 3:
+        ?>
+        <!-- Intro Banner -->
+        <div class="welcome_banner full_banner_colored centered">
+            <div class="content clearfix">
+                <?php if ($kyma_theme_options['info_title'] != "") { ?>
+                    <h3><?php echo esc_attr($kyma_theme_options['info_title']); ?></h3>
+                <?php }
+                if ($kyma_theme_options['info_description'] != "") { ?>
+                    <span class="intro_text"><?php echo esc_attr($kyma_theme_options['info_description']); ?></span>
+                <?php } ?>
+                <!-- <span class="rotate_icon"><i class="ico-lightbulb"></i></span> -->
+            </div>
+        </div>
+        <!-- End Intro Banner -->
+    <?php
+} ?>
