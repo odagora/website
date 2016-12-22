8735fea28b54ed2d82023053eeb8f7b9e8dde739
Daniel Gonzalez <oscar.d.gonzalez@gmail.com>
2016-12-22 17:22:50 -0500

    added blog-standard template modified and classes

---
+++ wp-content/themes/Kyma-child/blog-standard.php
@@ -0,0 +1,50 @@
+<?php
+/* Template Name: Blog Standard */
+get_header();
+get_template_part('breadcrumbs');
+the_post(); ?>
+    <!-- Our Blog Grids -->
+<?php $kyma_theme_options = kyma_theme_options(); ?>
+    <section class="content_section">
+        <div class="content row_spacer blog">
+            <?php
+            $blog_layout = get_post_meta(get_the_ID(), 'content_layout', true);
+            $imageSize = $blog_layout == "fullwidth" ? 'kyma_home_post_full_thumb' : 'kyma_home_post_thumb';
+            if ($blog_layout == "leftsidebar") {
+                get_sidebar();
+                $float = "f_right";
+            } elseif ($blog_layout == "fullwidth") {
+                $float = "";
+            } elseif ($blog_layout == "rightsidebar") {
+                $float = "f_left";
+            } else {
+                $blog_layout = "rightsidebar";
+                $float = "f_right";
+            } ?>
+            <!-- All Content -->
+            <div class="content_spacer clearfix">
+                <?php if ($blog_layout == "leftsidebar" || $blog_layout == "rightsidebar"){ ?>
+                <div class="content_block col-md-9 <?php echo esc_attr($float); ?> ">
+                    <?php } ?>
+                    <div class="hm_blog_list clearfix">
+                        <div <?php post_class('blog_grid_block clearfix'); ?>>
+                            <div class="blog_grid_con">
+                                <?php the_content(); ?>
+                            </div>
+                        </div>
+                        <!-- End blog post detail -->
+                    </div>
+                    <!-- End Content Block -->
+                    <?php if ($blog_layout == "leftsidebar" || $blog_layout == "rightsidebar"){ ?>
+                </div>
+            <?php
+            }
+            if ($blog_layout == "rightsidebar") {
+                get_sidebar();
+            } ?>
+                <!-- All Content -->
+            </div>
+        </div>
+    </section>
+    <!-- End Our Blog Grids -->
+<?php get_footer(); ?>
