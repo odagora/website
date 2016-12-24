eb5ecc0878775a00e8cede58a29ff8c60e16bfda
Daniel Gonzalez <oscar.d.gonzalez@gmail.com>
2016-12-24 10:59:29 -0500

    fixed Oauth.php bug in webserver

--- wp-content/themes/Kyma/functions/scripts/twitteroauth/OAuth.php
+++ wp-content/themes/Kyma/functions/scripts/twitteroauth/OAuth.php
@@ -3,8 +3,12 @@
 
 /* Generic exception class
  */
-class OAuthException extends Exception {
-  // pass
+if (!class_exists('OAuthException')) { 
+
+    class OAuthException extends Exception {
+          // pass
+    }
+
 }
 
 class OAuthConsumer {
