f4c49687cfcd832c0b55a23e2a964d07898b1189
Daniel <oscar.d.gonzalez@gmail.com>
2017-08-14 13:50:30 -0500

    added yoast seo plugin and pagination title and description feature

--- wp-content/themes/Kyma-child/functions.php
+++ wp-content/themes/Kyma-child/functions.php
@@ -48,4 +48,27 @@ function remove_the_wpautop_function() {
     remove_filter( 'the_excerpt', 'wpautop' );
 }
 add_action( 'after_setup_theme', 'remove_the_wpautop_function' );
+
+/** Add Page Number to Title and Meta Description for SEO **/
+if ( ! function_exists( 'multipage_metadesc' ) ){
+   function multipage_metadesc( $s ){
+      global $page;
+      $paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
+      ! empty ( $page ) && 1 < $page && $paged = $page;
+      $paged > 1 && $s .= ' - ' . sprintf( __( 'Parte %s' ), $paged );
+      return $s;
+   }
+   add_filter( 'wpseo_metadesc', 'multipage_metadesc', 100, 1 );
+}
+
+if ( ! function_exists( 'multipage_title' ) ){
+   function multipage_title( $title ){
+      global $page;
+      $paged = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
+      ! empty ( $page ) && 1 < $page && $paged = $page;
+      $paged > 1 && $title .= ' - ' . sprintf( __( 'Parte %s' ), $paged );
+      return $title;
+   }
+   add_filter( 'wpseo_title', 'multipage_title', 100, 1 );
+}
 ?>
