d3b6cad3ee99a6ea30d77a1e69bda310d808a3ab
Daniel <oscar.d.gonzalez@gmail.com>
2017-09-29 17:15:25 -0500

    added muti-step-form plugin and landing page layout with this form

--- wp-config.php
+++ wp-config.php
@@ -78,6 +78,7 @@ $table_prefix  = 'wp_';
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define('WP_DEBUG', false);
+define ('WPLANG', 'es_CO');
 /* Multisite */
 define( 'WP_ALLOW_MULTISITE', true );
 define('MULTISITE', true);
